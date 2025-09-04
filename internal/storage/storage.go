92b3ad8c4975d0deee1c470fae5f32cc23012160
kwame-Owusu <alfredowusu595@gmail.com>
2025-09-04 19:57:03 +0100

    wip, add function for saving todos to json file

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,10 +1,33 @@
 package storage
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/kwame-Owusu/todo-cli/internal/models"
 )
 
 func SaveTodos(todos *models.TodoList, filename string) error {
+	if !strings.HasSuffix(filename, ".json") {
+		return fmt.Errorf("Invalid filename, file has to end in .json")
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("Error occurred creating file: %s", err)
+	}
+	defer file.Close()
+
+	// write to json
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", " ") //make json pretty
+	err = encoder.Encode(todos.Todos)
+	if err != nil {
+		return fmt.Errorf("Error enconding todos: %s", err)
+	}
+	fmt.Printf("Todos saved in %s\n", filename)
 	return nil
 }
 
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/kwame-Owusu/todo-cli/internal/models"
+	"github.com/kwame-Owusu/todo-cli/internal/storage"
 )
 
 func main() {
 	todos := models.NewTodoList()
 	todos.Add("This is first Todo")
+	err := storage.SaveTodos(todos, "test.json")
+	if err != nil {
+		fmt.Printf("Error saving todos in main, %s", err)
+	}
 	fmt.Println(todos.List())
 }
