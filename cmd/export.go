e213c10d5e05af6890598c3a95b6718381ccf424
kwame-Owusu <alfredowusu595@gmail.com>
2026-05-27 21:03:27 +0100

    add export command, which can be used to export todos to .md files and other places that support .md syntax

--- cmd/export.go
+++ cmd/export.go
@@ -1,6 +1,13 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/kwame-Owusu/lista/internal/storage"
+	"github.com/spf13/cobra"
+)
 
 var exportCmd = &cobra.Command{
 	Use:   "export",
@@ -10,4 +17,29 @@ var exportCmd = &cobra.Command{
 	Run:   exportTodos,
 }
 
-func exportTodos(cmd *cobra.Command, args []string) {}
+func exportTodos(cmd *cobra.Command, args []string) {
+	todos, err := storage.LoadTodos(dataFile)
+	if err != nil {
+		fmt.Printf("Error reading todos %v", err)
+	}
+
+	var sb strings.Builder
+
+	sb.WriteString("# Lista\n\n")
+	sb.WriteString("## Todos\n")
+	for _, todo := range todos {
+		checked := " "
+		if todo.Completed {
+			checked = "x"
+		}
+		fmt.Fprintf(&sb, "- [%s] %s\n", checked, todo.Title)
+	}
+
+	sb.WriteString("\n")
+	sb.WriteString("## Notes\n")
+	for _, todo := range todos {
+		fmt.Fprintf(&sb, "- %s: %s\n", todo.Title, todo.Notes)
+	}
+
+	fmt.Fprint(os.Stdout, sb.String())
+}
